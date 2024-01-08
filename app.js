ceb6181b6d5d1a58dfba2e91db812fea43e4f7df
ZeenatFirdosh <zeenatzfq052@gmail.com>
2024-01-08 19:00:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+
+const errorMiddleware = require("./middleware/error");
+
+// Config
+
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+// Route Imports
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
+
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
+app.use("/api/v1", payment);
+
+app.use(express.static(path.resolve(__dirname, 'build')));
+// 
+// this line we add to make react router work in case of other routes doesnt match
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve('build', 'index.html'))
+);
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+// });
+
+// Middleware for Errors
+app.use(errorMiddleware);
+
+module.exports = app;
